974dcaa5c9c6b2f34ffcba448bfb256bdf446af1
Samir <samirb80440@gmail.com>
2024-06-25 15:07:57 +0200

    pdo

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "Afpa1234";
+$dbname = "record";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // configurer le mode d'erreur PDO pour générer des exceptions
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connecté avec succès à la base de données";
+} catch(PDOException $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
+}
+
+//ou, si vous utilisez une fonction, n'oubliez pas le `return`:
+
+function connect_database(){
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // configurer le mode d'erreur PDO pour générer des exceptions
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connecté avec succès à la base de données";
+
+        return $conn;
+
+    } catch(PDOException $e) {
+        echo "Erreur de connexion à la base de données: " . $e->getMessage();
+    }
+
+    }
+
+?>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Velvet record</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
