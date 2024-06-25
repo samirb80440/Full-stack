eab93f7e1d1069232a5d7a17c8a4950e1342742e
Samir <samirb80440@gmail.com>
2024-06-25 09:16:19 +0200

    PDO

--- details_disc.php
+++ details_disc.php
@@ -3,7 +3,7 @@
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $requete = $db->prepare("select * from disc where disc_id=?");
-    $requete->execute(array($_GET["disc_id"]));
+    $requete->execute(array($_POST["disc_id"]));
     $disc = $requete->fetch(PDO::FETCH_OBJ);
 ?>
 <html>
@@ -13,8 +13,9 @@
 </head>
 <html>
 <body>
+    <form method=POST><input type=text  name=disc_id ></form>
     Disc N° <?= $disc->disc_id ?>
-    Disc name <?= $disc->disc_name ?>
+    Disc name <?= $disc->disc_title ?>
     Disc year <?= $disc->disc_year ?>
 </body>
 </html>
