213abf66d90e1b66a330bafd9131b5ec62eca963
Samir <samirb80440@gmail.com>
2024-06-25 15:45:56 +0200

    pdo

--- details_disc.php
+++ details_disc.php
@@ -1,10 +1,18 @@
 <?php
-    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=record', 'admin', 'Afpa1234');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // Crée une nouvelle connexion à la base de données avec PDO
+  $db = new PDO('mysql:host=localhost;charset=utf8;dbname=record', 'admin', 'Afpa1234');
 
-    $requete = $db->prepare("select * from disc where disc_id=?");
-    $requete->execute(array($_GET["disc_id"]));
-    $disc = $requete->fetch(PDO::FETCH_OBJ);
+  // Définit le mode d'erreur pour afficher les erreurs SQL
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // Prépare une requête SQL pour sélectionner un disque en fonction de son ID
+  $requete = $db->prepare("select * from disc where disc_id=?");
+
+  // Exécute la requête en passant l'ID du disque en paramètre
+  $requete->execute(array($_GET["disc_id"]));
+
+  // Récupère le résultat de la requête sous forme d'objet
+  $disc = $requete->fetch(PDO::FETCH_OBJ);
 ?>
 <html>
 <head>
