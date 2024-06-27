7eaa786b6e43580ff63066b998a9dd7d38d0a946
Samir <samirb80440@gmail.com>
2024-06-27 09:07:24 +0200

    pdo

--- header.php
+++ header.php
@@ -6,16 +6,9 @@ $username = "admin";
 $password = "Afpa1234";
 $dbname = "record";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // configurer le mode d'erreur PDO pour générer des exceptions
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connecté avec succès à la base de données";
-} catch(PDOException $e) {
-    echo "Erreur de connexion à la base de données: " . $e->getMessage();
-}
 
-//ou, si vous utilisez une fonction, n'oubliez pas le `return`:
+
+// si vous utilisez une fonction, n'oubliez pas le `return`:
 
 function connect_database(){
 
@@ -32,9 +25,5 @@ function connect_database(){
     }
 
     }
-
-
-
-
 
 ?>  
