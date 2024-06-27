bf5621f2d6266213933749366bc330972e591e08
Samir <samirb80440@gmail.com>
2024-06-27 14:58:45 +0200

    pdo

--- exemple pdo/details_disc.php
+++ exemple pdo/details_disc.php
@@ -15,15 +15,21 @@
   $disc = $requete->fetch(PDO::FETCH_OBJ);
 ?>
 <html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Test PDO</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Record</title>
 </head>
 <html>
 <body>
-    <form method=GET><input type=text name=disc_id ></form>
-    Disc N° <?= $disc->disc_id ?>
-    Disc title <?= $disc->disc_title ?>
-    Disc year <?= $disc->disc_year ?>
+    <form method='GET'>
+    <label for='disc_id' >numéros de disque souhaiter</label><input type="textarea" row='4' cols='2' name='disc_id' id='disc_id'>
+    <button type="submit">envoyer</button>  
+    
+    <div>Disc N° <?= $disc->disc_id ?></div>
+    <div>Disc name <?= $disc->disc_title ?></div>
+    <div>Disc year <?= $disc->disc_year ?></div>
+    <img src='../pictures/<?php echo $disc->disc_picture;?>' width='200px' alt='...'>
 </body>
 </html>
