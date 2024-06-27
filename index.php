e479731a9cd33602ebb35f1a2d7672a91bdae96b
Samir <samirb80440@gmail.com>
2024-06-27 08:58:11 +0200

    pdo

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
-
-
-
-
-
+require_once('header.php')
 
 
 
@@ -20,3 +24,5 @@
 
 
 ?>
+</body>
+</html>
