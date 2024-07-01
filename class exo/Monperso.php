ac9d6ab8056ccff7d9b751c0fd19587320a247f4
Samir <samirb80440@gmail.com>
2024-07-01 15:19:38 +0200

    class

--- class exo/Monperso.php
+++ class exo/Monperso.php
@@ -8,6 +8,9 @@ $p->setPrenomage("Jeff");
 $p->setAge("30");
 $p->setGenre("masculin");
 
-var_dump($p);
+//var_dump($p);
+ foreach($p as $key =>$value){
+    echo $key." : ".$value."<br>";
+ };
 
 ?>
