97a64651dfd36b0a35e273fa51cd0202fd05b038
Samir <samirb80440@gmail.com>
2024-07-02 10:46:07 +0200

    class

--- class exo/monemploye.php
+++ class exo/monemploye.php
@@ -5,7 +5,7 @@ require_once('POO/classes/employe.php');
 $em = new Employe();
 $em->setNommage("Lebowski");
 $em->setPrenomage("Jeff");
-$em->setdateembauche("05/02/1999");
+$em->setdateembauche("10/02/1997");
 $em->setposte("Ouvrier");
 $em->setsalaire("1600");
 $em->setservice("Batiment");
