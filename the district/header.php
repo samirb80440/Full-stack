d0327f9dd36eafd8ac1fc69c999b2cd590eaab01
Samir <samirb80440@gmail.com>
2024-07-08 09:01:40 +0200

    rectif

--- the district/header.php
+++ the district/header.php
@@ -10,16 +10,16 @@
             <div class="collapse navbar-collapse justify-content-center col-6" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                     <li class="textnav nav-item px-5">
-                        <a  <?php if($_SERVER['REQUEST_URI'] =="/PHP.page/index.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="index.php" title="Accueil">Accueil</a>
+                        <a  <?php if($_SERVER['REQUEST_URI'] =="/index.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="index.php" title="Accueil">Accueil</a>
                     </li>
                     <li class="textnav nav-item px-5">
-                        <a <?php if($_SERVER['REQUEST_URI'] =="/PHP.page/Categorie.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Categorie.php" title="Categorie">Categorie</a>
+                        <a <?php if($_SERVER['REQUEST_URI'] =="/Categorie.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Categorie.php" title="Categorie">Categorie</a>
                     </li>
                     <li class="textnav nav-item px-5">
-                        <a <?php if($_SERVER['REQUEST_URI'] == "/PHP.page/Plat.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Plat.php" title="Plat">Plat</a>
+                        <a <?php if($_SERVER['REQUEST_URI'] == "/Plat.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Plat.php" title="Plat">Plat</a>
                     </li>
                     <li class="textnav nav-item px-5">
-                        <a <?php if($_SERVER['REQUEST_URI'] == "/PHP.page/contact.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="contact.php" title="Contact">Contact</a>
+                        <a <?php if($_SERVER['REQUEST_URI'] == "/contact.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="contact.php" title="Contact">Contact</a>
                     </li>
                 </ul>
             </div>
