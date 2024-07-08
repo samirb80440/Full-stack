2827d2bc5d545bcd16e7e55c80f50ab45d9e4873
Samir <samirb80440@gmail.com>
2024-07-08 16:37:51 +0200

    fonction pdo

---
+++ the district/DAO.php
@@ -0,0 +1,62 @@
+<?php
+
+function insert_plat($nv_plat,$checkimage){
+
+if($checkimage ==1){
+    $stock = 'plat';
+    stock_image($nv_plat['image'],$stock);
+}
+
+$stmt= $conn->prepare("INSERT INTO plat(libelle,description,prix,image,id_categorie,active)VALUES(:libelle,:description,:prix,:image,:id_categorie,:active;");
+
+
+$stmt->bindParam(':libelle',$nv_plat['libelle']);
+$stmt->bindParam(':descrition',$nv_plat['descrition']);
+$stmt->bindParam(':prix',$nv_plat['prix']);
+$stmt->bindParam(':image',$nv_plat['image']);
+$stmt->bindParam(':id_categorie',$nv_plat['id_categorie']);
+$stmt->bindParam(':libelle',$nv_plat['libelle']);
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
