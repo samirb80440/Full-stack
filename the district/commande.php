767bbbe20fefdf3a718cd41e40cb2c519cbf28fb
Samir <samirb80440@gmail.com>
2024-07-08 08:45:10 +0200

    rectification

--- the district/commande.php
+++ the district/commande.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/commande.css">
-    <link rel="shortcut icon" type="image/png" href="images_the_district/the_district_brand/favicon.png">
+    <link rel="shortcut icon" type="image/png" href="assets/images_the_district/the_district_brand/favicon.png">
     <title>District</title>
 </head>
 
