3749147ce276cbd2852484ed7f0662ef9d78e5a6
Samir <samirb80440@gmail.com>
2024-07-19 12:01:16 +0200

    test composer/phpmailer

---
+++ exemple pdo/testmail.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+// On va utiliser le SMTP
+$mail->isSMTP();
+
+// On configure l'adresse du serveur SMTP
+$mail->Host       = 'localhost';    
+
+// On désactive l'authentification SMTP
+$mail->SMTPAuth   = false;    
+
+// On configure le port SMTP (MailHog)
+$mail->Port       = 1025;                                   
+
+// Expéditeur du mail - adresse mail + nom (facultatif)
+$mail->setFrom('from@thedistrict.com', 'The District Company');
+
+// Destinataire(s) - adresse et nom (facultatif)
+$mail->addAddress("client1@example.com", "Mr Client1");
+$mail->addAddress("client2@example.com"); 
+
+//Adresse de reply (facultatif)
+$mail->addReplyTo("reply@thedistrict.com", "Reply");
+
+//CC & BCC
+$mail->addCC("cc@example.com");
+$mail->addBCC("bcc@example.com");
+
+// On précise si l'on veut envoyer un email sous format HTML 
+$mail->isHTML(true);
+
+// On ajoute la/les pièce(s) jointe(s)
+$mail->addAttachment('text.txt');
+
+// Sujet du mail
+$mail->Subject = 'Test PHPMailer';
+
+// Corps du message
+$mail->Body = "On teste l'envoi de mails avec PHPMailer";
+
+// On envoie le mail dans un block try/catch pour capturer les éventuelles erreurs
+if ($mail){
+    try {
+        $mail->send();
+        echo 'Email envoyé avec succès';
+        } catch (Exception $e) {
+        echo "L'envoi de mail a échoué. L'erreur suivante s'est produite : ", $mail->ErrorInfo;
+        }
+    }
