00ba31c12c2c29a869f61095dd7c53e8620efebe
Samir <samirb80440@gmail.com>
2024-07-30 08:51:02 +0200

    header active

--- the district class/header.php
+++ the district class/header.php
@@ -72,9 +72,9 @@ echo '<link rel="stylesheet" href="assets/css/' . $cssFile . '">'; ?>
                     <li class="textnav nav-item px-5">
                         <a <?php if($_SERVER['REQUEST_URI'] =="/Categorie.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Categorie.php" title="Categorie">Categorie</a>
                     </li>
-                    <li class="textnav nav-item px-5">
-                        <a <?php if($_SERVER['REQUEST_URI'] == "/Plat.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Plat.php" title="Plat">Plat</a>
-                    </li>
+                    <li class="nav-item px-5">
+                    <a <?php if($_SERVER['REQUEST_URI'] == "/Plat.php?numcat=".$_GET['numcat']."" || $_SERVER['REQUEST_URI'] == "/Plat.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="Plat.php" title="Plat">Plat</a>
+                </li>
                     <li class="textnav nav-item px-5">
                         <a <?php if($_SERVER['REQUEST_URI'] == "/contact.php")echo ' class="textnav nav-link active"';else echo ' class="textnav nav-link"'?> href="contact.php" title="Contact">Contact</a>
                     </li>
