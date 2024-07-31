3601c97ec44e5beddd15234418758426bff567ba
Samir <samirb80440@gmail.com>
2024-07-31 13:57:17 +0200

    contactscript

---
+++ the district class/contactscript.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+
+//echo 'la livraison de votre commande est estimer a  '.date('H:i:s', strtotime('+30 minutes', strtotime(date('H:i:s'))))." elle sera livrer au nom de ".$_REQUEST["nom"]." a l'adresse ".$_REQUEST["adresse"];
+
+$infoscommande = "\nnom:".$_REQUEST['nom']."prenom:".$_REQUEST['prenom'].", email :".$_REQUEST['Email'].", telephone :".$_REQUEST['tel'].",demande du client :".$_REQUEST['demande'].", date et heure de la commande :".date("d/m/Y H-m-s");
+
+     // Ouverture en écriture seule 
+     $fp = fopen("assets/text/AAA-MM-JJ-HH-MM.txt", "a"); 
+     
+     // Ecriture du contenu ($myVar) 
+     fputs($fp, $infoscommande); 
+     
+     // Fermeture du fichier  
+     fclose($fp);
+
+     header("location:index.php")
+?>
